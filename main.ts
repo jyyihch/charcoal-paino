066844f617ab7b66dc7bd6be356d91e1e8afbf90
jyyihch <cichen.tw@gmail.com>
2021-08-24 22:29:38 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,19 @@
-input.onButtonPressed(Button.A, function () {
-    music.playTone(frequency[Math.abs(pins.analogReadPin(AnalogPin.P1) / 128)], music.beat(BeatFraction.Half))
-})
 let frequency: number[] = []
-frequency = [
-262,
-294,
-330,
-349,
-392,
-440,
-494,
-523
-]
+let keynote = 0
+input.onButtonPressed(Button.A, function () {
+    frequency = [
+    262,
+    294,
+    330,
+    349,
+    392,
+    440,
+    494,
+    523
+    ]
+    keynote = pins.analogReadPin(AnalogPin.P1) / 128
+    music.playTone(frequency[Math.abs(keynote)], music.beat(BeatFraction.Half))
+})
 basic.forever(function () {
 	
 })
